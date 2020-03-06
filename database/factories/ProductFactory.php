16735f03cc50d4d43dbe6134f2627fe47398ed43
yokari-0000 <yokari.tsukahara@gmail.com>
2020-03-06 12:05:02 +0530

    faker update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,7 +9,7 @@ $factory->define(App\Product::class, function (Faker\Generator $faker) {
     return [
         'title' => $faker->sentence(3),
         'content' => $faker->paragraph(2),
-        'image' => '1583128731pexels-photo-cat.jpeg',
+        'image' => secure_asset('storage/cat1.jpeg'),
         'price' => $faker->numberBetween(100,10000),
     ];
 });
--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -12,23 +12,6 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        \App\Product::create([
-        'title' => 'Product 1',
-        'image' => secure_asset('storage/cat1.jpeg'),
-        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde quam non distinctio quasi. Autem nulla eveniet dolores pariatur possimus nisi accusamus, aspernatur vero sint nesciunt, ipsam odit repudiandae repellendus!',
-        'price' => '1000'
-        ]);
-        \App\Product::create([
-        'title' => 'Product 2',
-        'image' => secure_asset('storage/cat2.jpeg'),
-        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde quam non distinctio quasi. Autem nulla eveniet dolores pariatur possimus nisi accusamus, aspernatur vero sint nesciunt, ipsam odit repudiandae repellendus!',
-        'price' => '1000'
-        ]);
-       \App\Product::create([
-        'title' => 'Product 3',
-        'image' => secure_asset('storage/picture.jpeg'),
-        'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde quam non distinctio quasi. Autem nulla eveniet dolores pariatur possimus nisi accusamus, aspernatur vero sint nesciunt, ipsam odit repudiandae repellendus!',
-        'price' => '1000'
-        ]);
-        }
+        factory(App\Product::class,20)->create();
     }
+}
