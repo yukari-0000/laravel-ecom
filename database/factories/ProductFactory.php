d1700e5ccef534316beb95ee56594a59fa6bac0b
yokari-0000 <yokari.tsukahara@gmail.com>
2020-03-06 11:24:46 +0530

    faker update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,7 +9,7 @@ $factory->define(App\Product::class, function (Faker\Generator $faker) {
     return [
         'title' => $faker->sentence(3),
         'content' => $faker->paragraph(2),
-        'image' => $faker->url,
+        'image' => '1583128731pexels-photo-cat.jpeg',
         'price' => $faker->numberBetween(100,10000),
     ];
 });
