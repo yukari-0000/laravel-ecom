d39a861027af891c4bb1e7456aa71ed24e8d4648
yokari-0000 <yokari.tsukahara@gmail.com>
2020-03-06 11:37:26 +0530

    faker update

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -13,19 +13,19 @@ class ProductsTableSeeder extends Seeder
     public function run()
     {
         \App\Product::create([
-        'name' => 'Product 1',
+        'title' => 'Product 1',
         'image' => '1583128731pexels-photo-cat.jpeg',
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde quam non distinctio quasi. Autem nulla eveniet dolores pariatur possimus nisi accusamus, aspernatur vero sint nesciunt, ipsam odit repudiandae repellendus!',
         'price' => '1000'
         ]);
         \App\Product::create([
-        'name' => 'Product 2',
+        'title' => 'Product 2',
         'image' => '1583208818pexels-photo-cat2.jpeg',
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde quam non distinctio quasi. Autem nulla eveniet dolores pariatur possimus nisi accusamus, aspernatur vero sint nesciunt, ipsam odit repudiandae repellendus!',
         'price' => '1000'
         ]);
        \App\Product::create([
-        'name' => 'Product 3',
+        'title' => 'Product 3',
         'image' => '1583208971pexels-photo-3646213.jpeg',
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde quam non distinctio quasi. Autem nulla eveniet dolores pariatur possimus nisi accusamus, aspernatur vero sint nesciunt, ipsam odit repudiandae repellendus!',
         'price' => '1000'
