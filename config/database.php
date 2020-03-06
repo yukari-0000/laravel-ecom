bfbffad1d4e65195810a316cdd88d55adc0e3b64
yokari-0000 <yokari.tsukahara@gmail.com>
2020-03-06 10:33:08 +0530

    faker update

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$db = substr($url["path"], 1);
+$database = substr($url["path"], 1);
 
 
 return [
