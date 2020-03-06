51787c39b991de5ea9b84568fef70122072b7098
yokari-0000 <yokari.tsukahara@gmail.com>
2020-03-06 12:08:31 +0530

    faker update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,7 +5,7 @@
 use App\Product;
 use Faker\Generator as Faker;
 
-$factory->define(App\Product::class, function (Faker $faker) {
+$factory->define(App\Product::class, function (Faker\Generator\Generator $faker) {
     return [
         'title' => $faker->sentence(3),
         'content' => $faker->paragraph(2),
